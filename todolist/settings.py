cd2683a6973afcf58460d2d013ef6416b97a57ed
BOB <godbobkimo@yahoo.com.tw>
2022-07-31 17:02:06 +0800

    完成mysql&aws試試

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from re import template
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +86,13 @@ DATABASES = {
     }
 }
 
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'demo',
+    'USER': 'admin',
+    'PASSWORD': 'Fbsucks35',
+    'HOST': 'database-1.cf6upcqwfeju.us-west-2.rds.amazonaws.com',
+    'PORT': '3306', }}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
